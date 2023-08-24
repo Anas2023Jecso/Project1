aa671ba1f21663a11b291757e90433372583c819
DelThis786 <islamictv306@gmail.com>
2023-08-24 07:31:54 +0500

    half routing done

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import NIF from "./pages/nif";
 import NISS from "./pages/niss";
 
 
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <BankAccount />
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="nif" element={<NIF />} />
+    <Route path="niss" element={<NISS />} />
+    <Route path="fiscal" element={<Fiscal />} />
+    <Route path="bank" element={<BankAccount />} />
+    <Route path="*" element={<Navigate to="/" replace={true} />} />
+  </Routes>
     </div>
   );
 }
 
 export default App;
+
